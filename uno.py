0ae0f42a479cd14f1435b6bf6c2c4ba2c0cc49bf
Sterling Duncan <techedreal@gmail.com>
2025-09-21 18:33:29 -0500

    first commit

---
+++ uno.py
@@ -0,0 +1,9 @@
+q = 54
+j = 43
+p = q - j
+r = j - q
+# Ideally will compare the numbers. Ideally.
+if q > j:
+    print ("{q} is more than {j} by {p}")
+else:
+    print ("{j} is more than {q} by {r}")
